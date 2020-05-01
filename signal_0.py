2b9088eaf960d21630dae15dd1533c89e296bef7
yezhi <1309449109@qq.com>
2020-05-01 11:16:54 -0400

    add signal_0.py

---
+++ signal_0.py
@@ -0,0 +1,19 @@
+"""
+    信号方法处理僵尸
+"""
+
+import os
+import signal
+
+#处理子进程退出
+signal.signal(signal.SIGCHLD,signal.SIG_IGN)
+
+pid = os.fork()
+if pid < 0:
+    print("Error")
+elif pid == 0:
+    print("Child PID:",os.getpid())
+else:
+    while True:
+        pass
+
